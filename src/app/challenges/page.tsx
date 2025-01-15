1f3e0e36d248c9fdfacdc353835803bc1c277f83
muuo <anthonymuuo92@gmail.com>
2025-01-15 12:39:39 +0300

    protect route /challenges

--- src/app/challenges/page.tsx
+++ src/app/challenges/page.tsx
@@ -1,5 +1,43 @@
+"use client";
 import CreateH2h from "@/components/createh2h";
+import { supabase } from "@/lib/supabase";
+import { useState, useEffect } from "react";
+import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
+
 const Challenges = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        setUser(session?.user ?? null);
+      }
+    );
+
+    const checkUser = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      if (!session?.user) {
+        router.push("/");
+      } else {
+        setUser(session.user);
+      }
+    };
+
+    checkUser();
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+  }, [router]);
+
+  if (user === null) {
+    return <div>Cheking if youre logged in...</div>;
+  }
+
   return (
     <div className="m-4">
       <CreateH2h />
