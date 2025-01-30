f1c0ab8a99689fe33a13fbfb7b58b60c77d9227d
seanbaraka <seanbaraka@gmail.com>
2025-01-30 11:47:44 +0300

    fix stuff

--- src/app/fixtures/_components/fixtures.tsx
+++ src/app/fixtures/_components/fixtures.tsx
@@ -72,46 +72,47 @@ export default function Fixtures() {
 
   return (
     <div className="w-full max-w-md">
-      {data.map((match, index) => (
-        <div
-          key={index}
-          className={` flex mb-3 hover:shadow-lg transition-shadow ${index !== matches.length - 1 ? "border-b-[1px]" : ""} px-0`}
-        >
-          <div className="p-4">
-            <div className="flex items-center justify-between">
-              <div className="flex flex-col gap-1">
-                <div className=" space-y-2">
-                  <div className="text-xs font-bold  flex gap-1 ">
-                    <span className=" text-muted">
-                      {match.metadata.competition}
-                    </span>
-                    {match.metadata.date}
+      {data &&
+        data.map((match, index) => (
+          <div
+            key={index}
+            className={` flex mb-3 hover:shadow-lg transition-shadow ${index !== matches.length - 1 ? "border-b-[1px]" : ""} px-0`}
+          >
+            <div className="p-4">
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-1">
+                  <div className=" space-y-2">
+                    <div className="text-xs font-bold  flex gap-1 ">
+                      <span className=" text-muted">
+                        {match.metadata.competition}
+                      </span>
+                      {match.metadata.date}
+                    </div>
+                    {/* soccer  icon*/}
+                    <div className=" flex gap-2 font-semibold text-md items-center">
+                      ⚽ {match.title}
+                    </div>
                   </div>
-                  {/* soccer  icon*/}
-                  <div className=" flex gap-2 font-semibold text-md items-center">
-                    ⚽ {match.title}
+                  <div className="">
+                    <div className="text-sm text-muted font-semibold flex items-center  gap-2">
+                      Current Pot Price:{" "}
+                      <Image
+                        width="25"
+                        height="25"
+                        src="https://img.icons8.com/arcade/64/coins--v1.png"
+                        alt="coins--v1"
+                      />{" "}
+                      {/* {match.pot} */}
+                    </div>
                   </div>
-                </div>
-                <div className="">
-                  <div className="text-sm text-muted font-semibold flex items-center  gap-2">
-                    Current Pot Price:{" "}
-                    <Image
-                      width="25"
-                      height="25"
-                      src="https://img.icons8.com/arcade/64/coins--v1.png"
-                      alt="coins--v1"
-                    />{" "}
-                    {/* {match.pot} */}
+                  <div className="flex text-muted font-semibold">
+                    <p>Market: {}</p>
                   </div>
-                </div>
-                <div className="flex text-muted font-semibold">
-                  <p>Market: {}</p>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
