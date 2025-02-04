6ac1a08a20d8f3a7ccc191aa00307fdf3ce29e6c
benjamin-23 <kitongabenja34@gmail.com>
2025-02-04 20:19:28 +0300

    loading circle

--- src/components/accountpage.tsx
+++ src/components/accountpage.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -41,6 +42,7 @@ const AccountPage = () => {
   }
 
   const [userData, setUserData] = React.useState<UserData | null>(null);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -54,6 +56,7 @@ const AccountPage = () => {
           },
         });
         const responseData = await response.json();
+        setIsLoading(false);
         setUserData(responseData);
       } catch (error) {
         console.error("Error fetching user data:", error);
@@ -63,118 +66,129 @@ const AccountPage = () => {
     fetchUserData();
   }, []);
   console.log(userData, "userdata");
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
+  }
   return (
     <div className="">
-      <div className="space-y-2">
-        <div className="p-2 space-y-2 bg-accent border border-muted/20 rounded-lg">
-          <div className="flex items-center space-x-4 justify-between">
-            <div>
-              <h2 className="text-lg font-bold">{userData?.username}</h2>
-              <p className="text-sm text-gray-600">{userData?.phone}</p>
-            </div>
-            <div>
-              <Avatar className="w-8 h-8">
-                <AvatarImage src="/path/to/avatar.jpg" alt="User Avatar" />
-                <AvatarFallback className="bg-blue-500 text-white">
-                  AD
-                </AvatarFallback>
-              </Avatar>
-            </div>
-          </div>
-          <div className="space-y-4">
-            <div className="balance my-2 space-y-2">
-              <div className="flex items-center justify-between">
-                <div className="bal flex items-center gap-2">
-                  <span className="flex items-center text-gray-700">
-                    <Wallet className="w-5 h-5 mr-2 text-blue-500" />
-                    <span className="text-xl">Balance:</span>
-                  </span>
-                  <span className=" text-xl">{userData?.balance} FC</span>
-                </div>
-                <div className="limit text-xs">
-                  <span>Min. Withdraw Amount 500 FC</span>
-                </div>
+      {userData && (
+        <div className="space-y-2">
+          <div className="p-2 space-y-2 bg-accent border border-muted/20 rounded-lg">
+            <div className="flex items-center space-x-4 justify-between">
+              <div>
+                <h2 className="text-lg font-bold">{userData?.username}</h2>
+                <p className="text-sm text-gray-600">{userData?.phone}</p>
               </div>
-              <div className=" text-center">
-                <PayoutModal />
+              <div>
+                <Avatar className="w-8 h-8">
+                  <AvatarImage src="/path/to/avatar.jpg" alt="User Avatar" />
+                  <AvatarFallback className="bg-blue-500 text-white">
+                    AD
+                  </AvatarFallback>
+                </Avatar>
               </div>
             </div>
-            <div className="flex items-center justify-between">
-              <span className="flex items-center text-gray-700">
-                <Trophy className="w-5 h-5 mr-2 text-yellow-500" /> Participated
-                Challenges:
-              </span>
-              <span className="font-semibold">
-                {userData?.participatedChallenges}
-              </span>
-            </div>
-            <div className="flex items-center justify-between">
-              <span className="flex items-center text-gray-700">
-                <Trophy className="w-5 h-5 mr-2 text-blue-500" /> Wins:
-              </span>
-              <span className="font-semibold">
-                {userData?.participatedChallenges}
-              </span>
-            </div>
-            <div className="flex items-center justify-between">
-              <span className="flex items-center text-gray-700">
-                <XCircle className="w-5 h-5 mr-2 text-red-500" /> Losses:
-              </span>
-              <span className="font-semibold"> {userData?.losses}</span>
-            </div>
-          </div>
-          <div className="my-2">
-            <div className="rounded-lg">
-              <h2 className="text-lg font-semibold text-gray-800">
-                Achieve this to withdraw
-              </h2>
-              <div className="space-y-3">
-                <div>
-                  <p className="text-gray-700">
-                    Earn ksh 1000 to be able to withdraw
-                  </p>
-                  <div className="w-full bg-gray-200 rounded-full h-2 mt-1">
-                    <div
-                      className="bg-green-500 h-2 rounded-full"
-                      style={{ width: "15%" }}
-                    ></div>
+            <div className="space-y-4">
+              <div className="balance my-2 space-y-2">
+                <div className="flex items-center justify-between">
+                  <div className="bal flex items-center gap-2">
+                    <span className="flex items-center text-gray-700">
+                      <Wallet className="w-5 h-5 mr-2 text-blue-500" />
+                      <span className="text-xl">Balance:</span>
+                    </span>
+                    <span className=" text-xl">{userData?.balance} FC</span>
                   </div>
-                  <p className="text-sm text-gray-500 mt-1">Ksh145 Earned</p>
+                  <div className="limit text-xs">
+                    <span>Min. Withdraw Amount 500 FC</span>
+                  </div>
+                </div>
+                <div className=" text-center">
+                  <PayoutModal />
+                </div>
+              </div>
+              <div className="flex items-center justify-between">
+                <span className="flex items-center text-gray-700">
+                  <Trophy className="w-5 h-5 mr-2 text-yellow-500" />{" "}
+                  Participated Challenges:
+                </span>
+                <span className="font-semibold">
+                  {userData?.participatedChallenges}
+                </span>
+              </div>
+              <div className="flex items-center justify-between">
+                <span className="flex items-center text-gray-700">
+                  <Trophy className="w-5 h-5 mr-2 text-blue-500" /> Wins:
+                </span>
+                <span className="font-semibold">
+                  {userData?.participatedChallenges}
+                </span>
+              </div>
+              <div className="flex items-center justify-between">
+                <span className="flex items-center text-gray-700">
+                  <XCircle className="w-5 h-5 mr-2 text-red-500" /> Losses:
+                </span>
+                <span className="font-semibold"> {userData?.losses}</span>
+              </div>
+            </div>
+            <div className="my-2">
+              <div className="rounded-lg">
+                <h2 className="text-lg font-semibold text-gray-800">
+                  Achieve this to withdraw
+                </h2>
+                <div className="space-y-3">
+                  <div>
+                    <p className="text-gray-700">
+                      Earn ksh 1000 to be able to withdraw
+                    </p>
+                    <div className="w-full bg-gray-200 rounded-full h-2 mt-1">
+                      <div
+                        className="bg-green-500 h-2 rounded-full"
+                        style={{ width: "15%" }}
+                      ></div>
+                    </div>
+                    <p className="text-sm text-gray-500 mt-1">Ksh145 Earned</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="bg-gray-100 p-2 rounded-lg max-w-md w-full space-y-2">
-          <div className="bg-white p-4 rounded-lg shadow-sm">
-            <p className="text-gray-800 font-medium mb-2">Your Referral Link</p>
-            <div className="flex items-center justify-between border border-gray-300 rounded-md px-3 py-2">
-              <span className="text-xs text-gray-600 truncate">
-                https://fantitk.com/auth?referral-code=12ICKGLG
-              </span>
-              <button className="text-blue-600 text-sm font-medium hover:underline">
-                <Copy className="w-5 h-5" />
-              </button>
+          <div className="bg-gray-100 p-2 rounded-lg max-w-md w-full space-y-2">
+            <div className="bg-white p-4 rounded-lg shadow-sm">
+              <p className="text-gray-800 font-medium mb-2">
+                Your Referral Link
+              </p>
+              <div className="flex items-center justify-between border border-gray-300 rounded-md px-3 py-2">
+                <span className="text-xs text-gray-600 truncate">
+                  https://fantitk.com/auth?referral-code=12ICKGLG
+                </span>
+                <button className="text-blue-600 text-sm font-medium hover:underline">
+                  <Copy className="w-5 h-5" />
+                </button>
+              </div>
+            </div>
+
+            <div className="bg-white p-4 rounded-lg shadow-sm mb-4">
+              <div className="flex items-center justify-between mb-2">
+                <p className="text-gray-800 font-medium">Your Referral Code</p>
+                <span className="text-gray-600 text-sm">
+                  {userData?.referralCode}
+                </span>
+              </div>
+              <div className="flex items-center justify-between">
+                <p className="text-gray-800 font-medium">Total Referrals</p>
+                <span className="text-gray-600 text-sm">
+                  {userData?.referralCount}
+                </span>
+              </div>
             </div>
           </div>
-
-          <div className="bg-white p-4 rounded-lg shadow-sm mb-4">
-            <div className="flex items-center justify-between mb-2">
-              <p className="text-gray-800 font-medium">Your Referral Code</p>
-              <span className="text-gray-600 text-sm">
-                {userData?.referralCode}
-              </span>
-            </div>
-            <div className="flex items-center justify-between">
-              <p className="text-gray-800 font-medium">Total Referrals</p>
-              <span className="text-gray-600 text-sm">
-                {userData?.referralCount}
-              </span>
-            </div>
-          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
